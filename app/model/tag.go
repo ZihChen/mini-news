de6325e539581e114d0798ad6747c83948886577
ZihChen <bronzii.max@gmail.com>
2022-10-11 15:00:15 +0800

    [fix] add user_id to Tag table

--- app/model/tag.go
+++ app/model/tag.go
@@ -5,6 +5,7 @@ import "time"
 type Tag struct {
 	ID        int       `json:"id" gorm:"column:id;type:int(11) unsigned auto_increment;NOT NULL;primary_key"`
 	Name      string    `json:"name" gorm:"column:name;type:varchar(255);not null"`
+	UserID    int       `json:"user_id" gorm:"column:user_id;type:int(11);NOT NULL"`
 	CreatedAt time.Time `json:"created_at" gorm:"column:created_at;type:TIMESTAMP; default:CURRENT_TIMESTAMP"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"column:updated_at;type:TIMESTAMP; not null;default:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"`
 }
